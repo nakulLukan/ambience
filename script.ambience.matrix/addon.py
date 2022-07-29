1ecc719575198f2839b273ebc0dfa97c4a6b88a8
Nakul A <matrixlukan@gmail.com>
2022-07-29 11:12:03 +0530

    Websocket communication integrated

--- script.ambience.matrix/addon.py
+++ script.ambience.matrix/addon.py
@@ -1,20 +1,28 @@
 import xbmcaddon
 import xbmcgui
 import xbmc
+from websocket import create_connection
 
 if ( __name__ == "__main__" ):
     i = 1
     monitor = xbmc.Monitor()
     capture = xbmc.RenderCapture()
-    fmt = capture.getImageFormat()
-    xbmcgui.Dialog().notification("Test", fmt)
+    dialog = xbmcgui.Dialog()
+    image_format = capture.getImageFormat()
+    dialog.notification("Image format", image_format)
+
+    ws = create_connection("ws://localhost:1880/ws/ambience")
+    dialog.notification("Web socket status", str(ws.getstatus()))
 
     while not monitor.abortRequested():
-        xbmc.sleep(5000)
+        xbmc.sleep(2000)
         i=i+1
         capture.capture(100,100)
         image = capture.getImage()
         count = len(image)
-        xbmcgui.Dialog().notification("Test", str(i) + str(count))
-    
+        dialog.notification("Data", str(i) + str(count))
+        ws.send("Hello, World")
+
+
     xbmcgui.Dialog().notification("Aborted", str(i))
+    ws.close()

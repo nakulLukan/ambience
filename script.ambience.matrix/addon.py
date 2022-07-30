30da39e5df0e02faa765833e98d8858aba68a387
Nakul A <matrixlukan@gmail.com>
2022-07-30 16:03:32 +0530

    - Logo created - Notification icon added - Code optimized - pixel value set to 32

--- script.ambience.matrix/addon.py
+++ script.ambience.matrix/addon.py
@@ -1,26 +1,54 @@
-from operator import le
 import xbmcgui
 import xbmc
 import json
+import xbmcaddon
 from websocket import create_connection
 
-if ( __name__ == "__main__" ):
+addon_icon_path = ''
 
+def send_pixel(ws, R, G, B, A):
+    ws.send(json.dumps({
+            "R": int(R),
+            "G": int(G),
+            "B": int(B),
+            "A": int(A)
+        }))
+
+def notify(dialog: xbmcgui.Dialog, heading: str, description: str):
+    dialog.notification(heading, description, addon_icon_path)
+
+if ( __name__ == "__main__" ):
+    addon = xbmcaddon.Addon()
     monitor = xbmc.Monitor()
     capture = xbmc.RenderCapture()
     dialog = xbmcgui.Dialog()
-    image_format = capture.getImageFormat()
-    dialog.notification("Image format", image_format)
-    ws = create_connection("ws://raspberrypi:1880/ws/ambience")
-    dialog.notification("Web socket status", str(ws.getstatus()))
+
+    addon_icon_path = addon.getAddonInfo('path') + '/icon.png'
+    notify(dialog, "Ambience", 'Activated')
+    player = xbmc.Player()
+
+    ws = create_connection("ws://raspberrypi.local:1880/ws/ambience")
+    notify(dialog, "Ambience", 'Connected to module successfully.')
+    pixel = 32
+    was_playing = False
     while not monitor.abortRequested():
-        xbmc.sleep(50)
-        capture.capture(100,100)
-        image = capture.getImage(50)
+        xbmc.sleep(20)
+        is_playing = player.isPlayingVideo()
+        if not is_playing:
+            if was_playing:
+                send_pixel(ws, 0, 0, 0, 0)
+                was_playing = False
+            continue
+        
+        was_playing = True
+
+        capture.capture(int(pixel * capture.getAspectRatio()),pixel)
+        image = capture.getImage(20)
         image_len = len(image)
         hasImage = image_len > 0
         if not hasImage:
             continue
+
         B = 0
         G = 0
         R = 0
@@ -30,6 +58,7 @@ if ( __name__ == "__main__" ):
         g = 1
         r = 2
         a = 3
+
         iteration = image_len / 4
         while r < iteration:
             R += (image[r])
@@ -41,12 +70,12 @@ if ( __name__ == "__main__" ):
             g = g + 4
             b = b + 4
             a = a + 4
-        ws.send(json.dumps({
-            "R": int(R / iteration),
-            "G": int(G / iteration),
-            "B": int(B / iteration),
-            "A": int(A / iteration)
-        }))
+            
+        send_pixel(ws,
+            R / iteration, 
+            G / iteration,
+            B / iteration,
+            A / iteration)
 
-    xbmcgui.Dialog().notification("Aborted", "XBMC monitor aborted'")
+    notify(dialog, "Aborted", "XBMC monitor aborted")
     ws.close()
--- script.ambience.matrix/readme.txt
+++ script.ambience.matrix/readme.txt
@@ -1,13 +1,11 @@
-This is a hello world popup tutorial for Kodi.
+# Ambience (addon)
 
-It is meant to be used as a guide to making your first Kodi Add-on.
+Connect kodi video to 'Ambience' backend. Ambience projects with dynamic lighting.
 
 ============
 Installation
 ============
 
-Launch Kodi >> Add-ons >> Get More >> .. >> Install from zip file
-
-Feel free to ask any questions on the Kodi Forums.
-
-Enjoy!
+1. Launch Kodi >> Add-ons >> Install from zip file >> select 'script.ambience.matrix.zip'.
+2. Start the addon from 'Programs add-ons'.
+3. Play some video in kodi and you can see the dynamic lighting projected by the abience backend module.
